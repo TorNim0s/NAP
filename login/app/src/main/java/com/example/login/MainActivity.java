1439db188ad9eb5f078af442f2d5fc76a365c225
Ilan <ilan1il1000@gmail.com>
2022-11-24 13:27:58 +0200

    button functions

--- login/app/src/main/java/com/example/login/MainActivity.java
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
+        MaterialButton signup = (MaterialButton) findViewById(R.id.signup);
+
         //admin admin
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +37,14 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     Toast.makeText(MainActivity.this, "LOGIN FAILED!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
             }
         });
     }
--- login/app/src/main/java/com/example/login/SignUpActivity.java
+++ login/app/src/main/java/com/example/login/SignUpActivity.java
@@ -2,7 +2,11 @@ package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        MaterialButton back = (MaterialButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
