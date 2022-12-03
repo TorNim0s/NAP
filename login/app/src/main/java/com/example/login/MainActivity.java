0f7576fb509d4f84f05118e1c4d9add31152b97e
Eldad Tsemach <eldad@tomeer.com>
2022-12-03 22:05:21 +0200

    delete staff

--- login/app/src/main/java/com/example/login/MainActivity.java
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ScrollView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
