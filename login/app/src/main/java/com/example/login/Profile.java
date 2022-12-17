37d992a7c867437b30da65c1c4bfd003ad8207b4
danazo <danazo246@gmail.com>
2022-12-17 21:09:31 +0200

    connect the profile screen to user data from the data base + changed the profile info from first name and last name to name and number

--- login/app/src/main/java/com/example/login/Profile.java
+++ login/app/src/main/java/com/example/login/Profile.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Profile extends AppCompatActivity {
 
@@ -36,6 +44,49 @@ public class Profile extends AppCompatActivity {
                 finish();
             }
         });
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DocumentReference userDocRef = firestore.collection("Users").document(user.getUid());
+
+        userDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // the user document exists, so we can retrieve the data
+                        String name = document.getString("name");
+                        String email = document.getString("email");
+                        String number = document.getString("number");
+
+                        // now we can display the data in the appropriate views
+                        TextView nameTextView = findViewById(R.id.name_text_view);
+                        nameTextView.setText(name);
+
+                        TextView numberTextView = findViewById(R.id.number_text_view);
+                        numberTextView.setText(number);
+
+                        TextView emailTextView = findViewById(R.id.email_text_view);
+                        emailTextView.setText(email);
+                        // now we can display the data in the appropriate views
+                    } else {
+                        // the user document doesn't exist, so we should show a Toast message and redirect the user to the login screen
+                        Toast.makeText(Profile.this, "User data not found", Toast.LENGTH_SHORT).show();
+                        Intent loginIntent = new Intent(Profile.this, SignIn.class);
+                        startActivity(loginIntent);
+                        finish();
+                    }
+                } else {
+                    // there was an error retrieving the document, so we should show a Toast message
+                    Toast.makeText(Profile.this, "Error retrieving user data", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+
 
     }
 
